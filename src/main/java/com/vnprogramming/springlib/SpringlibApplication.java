3ec0788db2dc46456278ea5393b470ee587d5ac9
Pham Van Khanh <rskvp93@gmail.com>
2024-01-03 21:19:19 +0700

    first commit

---
+++ src/main/java/com/vnprogramming/springlib/SpringlibApplication.java
@@ -0,0 +1,13 @@
+package com.vnprogramming.springlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnprogramming/springlib/SpringlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnprogramming.springlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
